0babb7c88dbd85710b9b46a8ab3e779512f8ba4c
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-04 09:33:13 +0200

    add files

--- Topics/Arithmetic operations/Slice and dice your price tag/src/Task.java
+++ Topics/Arithmetic operations/Slice and dice your price tag/src/Task.java
@@ -3,10 +3,6 @@
 public class Task {
   public static void main(String[] args) {
     // put your code here
-    int a = 5 + 7;
-    int b = a * 3;
-    int c = b / 4;
-    int d = b % c;
-    System.out.println(d);
+
   }
 }
