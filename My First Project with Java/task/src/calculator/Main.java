aaebeb665d71a2774e3f4e2b6b73cbe8ed94793f
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-04 10:04:20 +0200

    adding files

--- My First Project with Java/task/src/calculator/Main.java
+++ My First Project with Java/task/src/calculator/Main.java
@@ -9,13 +9,35 @@ public class Main {
         double doughnut = 2.5;
         double pancake = 3.2;
 
-        System.out.println("Prices:");
-        System.out.println("Bubblegum: $" + bubblegum);
-        System.out.println("Toffee: $" + toffee);
-        System.out.println("Ice cream: $" + iceCream);
-        System.out.println("Milk chocolate: $" + milkChocolate);
-        System.out.println("Doughnut: $" + doughnut);
-        System.out.println("Pancake: $" + pancake);
+//        System.out.println("Prices:");
+//        System.out.println("Bubblegum: $" + bubblegum);
+//        System.out.println("Toffee: $" + toffee);
+//        System.out.println("Ice cream: $" + iceCream);
+//        System.out.println("Milk chocolate: $" + milkChocolate);
+//        System.out.println("Doughnut: $" + doughnut);
+//        System.out.println("Pancake: $" + pancake);
+
+        int bubblegumTotal = 202;
+        int toffeeTotal = 118;
+        int iceCreamTotal = 2250;
+        int milkChocolateTotal = 1680;
+        int doughnutTotal = 1075;
+        int pancakeTotal = 80;
+
+        // Calculate total income
+        int totalIncome = bubblegumTotal + toffeeTotal + iceCreamTotal + milkChocolateTotal + doughnutTotal + pancakeTotal;
+
+        // Print the earned amounts
+        System.out.println("Earned amount:");
+        System.out.println("Bubblegum: $" + bubblegumTotal);
+        System.out.println("Toffee: $" + toffeeTotal);
+        System.out.println("Ice cream: $" + iceCreamTotal);
+        System.out.println("Milk chocolate: $" + milkChocolateTotal);
+        System.out.println("Doughnut: $" + doughnutTotal);
+        System.out.println("Pancake: $" + pancakeTotal);
+
+        // Print the total income
+        System.out.println("Income: $" + totalIncome);
 
     }
 }
---
+++ Topics/Naming variables/From cryptic to crystal  Weather's facelift/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int airTemperature = 25;
+        int airHumidity = 60;
+        int windSpeed = 6;
+
+        // TODO: Refactor variable names and print the results
+        System.out.println("airTemperature = " + airTemperature);
+        System.out.println("airHumidity = " + airHumidity);
+        System.out.println("windSpeed = " + windSpeed);
+    }
+}
