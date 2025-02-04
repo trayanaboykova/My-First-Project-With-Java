0b70b477106767bf7000217d51a2ac48807bcf87
Trayana Boykova <trayana.boykova@gmail.com>
2025-02-04 09:13:00 +0200

    adding files

--- My First Project with Java/task/src/calculator/Main.java
+++ My First Project with Java/task/src/calculator/Main.java
@@ -2,6 +2,20 @@ package calculator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int bubblegum = 2;
+        double toffee = 0.2;
+        int iceCream = 5;
+        int milkChocolate = 4;
+        double doughnut = 2.5;
+        double pancake = 3.2;
+
+        System.out.println("Prices:");
+        System.out.println("Bubblegum: $" + bubblegum);
+        System.out.println("Toffee: $" + toffee);
+        System.out.println("Ice cream: $" + iceCream);
+        System.out.println("Milk chocolate: $" + milkChocolate);
+        System.out.println("Doughnut: $" + doughnut);
+        System.out.println("Pancake: $" + pancake);
+
     }
 }
---
+++ Topics/Arithmetic operations/Calculating square, cube and fourth power of a number/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+        // Create a Scanner object to read input
+        Scanner in = new Scanner(System.in);
+
+        // Read the next integer
+        int n = in.nextInt(); 
+
+        // Compute and print the square of n
+
+        // Compute and print the cube of n
+
+        // Compute and print the fourth power of n
+    }
+}
